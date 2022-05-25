abf3d9a3003396214581f33f8cb8d4166bb4e71c
quangTD <tsquang398@gmail.com>
2022-05-26 06:53:51 +0700

    :lipstick: setup MUI and datagrid

---
+++ src/data/dataTable.mock.js
@@ -0,0 +1,178 @@
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className='cellWithImg'>
+          <img className='cellImg' src={params.row.img} alt='avatar' />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "age",
+    headerName: "Age",
+    width: 100,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      return (
+        <div className={`cellWithStatus ${params.row.status}`}>
+          {params.row.status}
+        </div>
+      );
+    },
+  },
+];
+
+//temporary data
+export const userRows = [
+  {
+    id: 1,
+    username: "Snow",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "active",
+    email: "1snow@gmail.com",
+    age: 35,
+  },
+  {
+    id: 2,
+    username: "Jamie Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "2snow@gmail.com",
+    status: "passive",
+    age: 42,
+  },
+  {
+    id: 3,
+    username: "Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "3snow@gmail.com",
+    status: "pending",
+    age: 45,
+  },
+  {
+    id: 4,
+    username: "Stark",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "4snow@gmail.com",
+    status: "active",
+    age: 16,
+  },
+  {
+    id: 5,
+    username: "Targaryen",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "5snow@gmail.com",
+    status: "passive",
+    age: 22,
+  },
+  {
+    id: 6,
+    username: "Melisandre",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "6snow@gmail.com",
+    status: "active",
+    age: 15,
+  },
+  {
+    id: 7,
+    username: "Clifford",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "7snow@gmail.com",
+    status: "passive",
+    age: 44,
+  },
+  {
+    id: 8,
+    username: "Frances",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "8snow@gmail.com",
+    status: "active",
+    age: 36,
+  },
+  {
+    id: 9,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "snow@gmail.com",
+    status: "pending",
+    age: 65,
+  },
+  {
+    id: 10,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "snow@gmail.com",
+    status: "active",
+    age: 65,
+  },
+];
+
+export const userRows2 = {
+  page: 1,
+  per_page: 6,
+  total: 12,
+  total_pages: 2,
+  data: [
+    {
+      id: 1,
+      email: "george.bluth@reqres.in",
+      first_name: "George",
+      last_name: "Bluth",
+      avatar: "https://reqres.in/img/faces/1-image.jpg",
+    },
+    {
+      id: 2,
+      email: "janet.weaver@reqres.in",
+      first_name: "Janet",
+      last_name: "Weaver",
+      avatar: "https://reqres.in/img/faces/2-image.jpg",
+    },
+    {
+      id: 3,
+      email: "emma.wong@reqres.in",
+      first_name: "Emma",
+      last_name: "Wong",
+      avatar: "https://reqres.in/img/faces/3-image.jpg",
+    },
+    {
+      id: 4,
+      email: "eve.holt@reqres.in",
+      first_name: "Eve",
+      last_name: "Holt",
+      avatar: "https://reqres.in/img/faces/4-image.jpg",
+    },
+    {
+      id: 5,
+      email: "charles.morris@reqres.in",
+      first_name: "Charles",
+      last_name: "Morris",
+      avatar: "https://reqres.in/img/faces/5-image.jpg",
+    },
+    {
+      id: 6,
+      email: "tracey.ramos@reqres.in",
+      first_name: "Tracey",
+      last_name: "Ramos",
+      avatar: "https://reqres.in/img/faces/6-image.jpg",
+    },
+  ],
+  support: {
+    url: "https://reqres.in/#support-heading",
+    text: "To keep ReqRes free, contributions towards server costs are appreciated!",
+  },
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,66 @@
-import React from "react";
+import "./home.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { userColumns, userRows, userRows2 } from "../../data/dataTable.mock";
 import { useContactsQuery } from "../../services/contactsApi";
 
 const Home = () => {
-  const { data } = useContactsQuery();
-  console.log("🚀TCL: ~ file: Home.js ~ line 6 ~ Home ~ data", data);
-  return <div>Home</div>;
+  // const { data, isLoading, error } = useContactsQuery();
+  // const [data, setData] = useState(userRows);
+  const [data, setData] = useState(userRows2.data);
+
+  // const rows = [
+  //   { id: 1, col1: "Hello", col2: "World" },
+  //   { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+  //   { id: 3, col1: "MUI", col2: "is Amazing" },
+  // ];
+
+  // const columns = [
+  //   { field: "col1", headerName: "Column 1", width: 150 },
+  //   { field: "col2", headerName: "Column 2", width: 150 },
+  // ];
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className='cellAction'>
+            <Link to={"/users/test"} style={{ textDecoration: "none" }}>
+              <div className='viewButton'>View</div>
+            </Link>
+            <div className='deleteButton'>Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className='datatable'>
+      <div className='datatable__title'>
+        Add New User
+        <Link to={"/users/new"} className='datatable__link'>
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        className='datatable__datagrid'
+        rows={data}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+      {/* <h1>Home</h1>
+      <div style={{ height: 300 }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div> */}
+    </div>
+  );
 };
 
 export default Home;
